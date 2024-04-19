1987becf9487e2e45aea1bf009dc337241736d24
Victor Stepin <vicstep1994@gmail.com>
2024-04-20 01:03:21 +0700

    Add file reading

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # FalloutHackingMicrogame
 My python implementation of Fallout hacking microgame
+
+Для корректной работы приложения необходимо создать файл words.txt в директории проекта и прописать слова, которые будут использованы во время игры.
+
+Пример содержимого файла words.txt:
+
+TOM
+BOM
+ROT
+GOT
+ZOD
+MUD
+FAG
--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 from random import randrange
 
 def create_words():
-    words = ["Tom", "Pot", "Rot"]
+    words = read_file_lines("words.txt")
+    words = [word.strip('\n') for word in words]
     return words
+
+def read_file_lines(file_path):
+    lines = None
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+    return lines
 
 def choose_secret_word(words):
     random_index = randrange(0, len(words))
-    wordToGuess = words[random_index]
-    return wordToGuess
+    word_to_guess = words[random_index]
+    return word_to_guess
 
 print("Welcome to Fallout Hacking Microgame")
 
 words = create_words()
 secret_word = choose_secret_word(words)
 print(words)
+print(secret_word)
 
 win = False
 attempts_count = 4
